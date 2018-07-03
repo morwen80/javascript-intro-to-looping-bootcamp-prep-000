7a3f3ed360047283af68c2599ca1d7916cc1e675
Valeria Ragonese <ribesnero80@gmail.com>
2018-07-03 15:14:54 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,15 +1,15 @@
-function forLoop(array){
-  for (var i = 0; i <= 25; i++) {
+// function forLoop(array){
+//   for (var i = 0; i <= 25; i++) {
 
-  if (i === 1) {
-    return ("I am 1 strange loop.")
-  }
-  else {
-    return ("I am " + i + " strange loops.");
-  }
+//   if (i === 1) {
+//     return ("I am 1 strange loop.")
+//   }
+//   else {
+//     return ("I am " + i + " strange loops.");
+//   }
 
-}
-}
+// }
+// }
 
 function whileLoop(number){
   while (number > 0) {
